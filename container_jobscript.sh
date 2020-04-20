fa45998d03b2adb64382c18364005c884f0db1b3
Brett Milash <brett.milash@utah.edu>
2020-04-20 13:13:14 -0600

    Singularity.local: downgraded openssl in star_env for samtools (libcrypto.so.1.0.0) container_jobscript.sh - trying to create slurm script for snakemake slurm jobs.

--- container_jobscript.sh
+++ container_jobscript.sh
@@ -1,9 +1,16 @@
 #!/bin/sh
 # Custom jobscript for containerized workflow.
 # properties = {properties}
-echo "$? command $*"
-env | grep "^SINGULARITY_NAME=" > /dev/null
-if [ -d /opt/conda ]
+#echo "$0 command $*"
+cp $0 jobscript.$SLURMD_NODENAME.$SLURM_JOBID
+#if [ -d /opt/conda ]
+echo "/etc/os-release:"
+cat /etc/os-release
+echo "PATH:"
+echo $PATH
+echo $PATH | grep "/opt/conda" > /dev/null
+#if [ -d /opt/conda ]
+if [ $? -eq 0 ]
 then
 # Inside container.
 echo "$0: Executing command inside container."
@@ -11,8 +18,14 @@ echo "$0: Executing command inside container."
 else
 # Outside container.
 echo "$0: Executing command from outside container."
-env
-module load singularity
+#env
+which singularity > /dev/null
+if [ $? -eq 1 ]
+then
+	module load singularity
+else
+	echo "Singularity already loaded."
+fi
 singularity shell rnaseq_slurm.sif <<END_OF_INPUT
 {exec_job}
 END_OF_INPUT
