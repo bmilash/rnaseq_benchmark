9f34c22ee484c817cd8c200107a56ae5773fda20
Brett Milash <brett.milash@utah.edu>
2022-07-19 16:47:34 -0600

    Updated for Rocky Linux 8.

--- run_workflow.sh
+++ run_workflow.sh
@@ -22,12 +22,14 @@ fi
 which snakemake >/dev/null 2>&1
 if [ $? -ne 0 ]
 then
-	module load snakemake/5.6.0
+	module load snakemake/6.4.1
 fi
 
-configfile=$2
+#configfile=$2
+configfile=config.yaml
 # Check if cluster config includes a reservation.
-cluster_config=$4
+#cluster_config=$4
+cluster_config=cluster.yaml
 grep reservation $cluster_config > /dev/null
 if [ $? = 0 ]
 then
@@ -37,12 +39,12 @@ else
 fi
 
 # Run snakemake.
-#snakemake -s $scriptdir/Snakefile.benchmark \
-#	--cluster-config $cluster_config \
-#	--configfile $configfile \
-#	--latency-wait 20 \
-#	--cluster "sbatch --clusters={cluster.cluster} --account={cluster.account} --partition={cluster.partition} $reservation --ntasks={cluster.ntasks} --time={cluster.time} -J '{rule}'" \
-#	--jobs 8 \
-#	$target
+snakemake -s $scriptdir/Snakefile.benchmark \
+	--cluster-config $cluster_config \
+	--configfile $configfile \
+	--latency-wait 20 \
+	--cluster "sbatch --clusters={cluster.cluster} --account={cluster.account} --partition={cluster.partition} $reservation --ntasks={cluster.ntasks} --time={cluster.time} -J '{rule}'" \
+	--jobs 8 \
+	$target
 
 date +'Finished at %R.'
